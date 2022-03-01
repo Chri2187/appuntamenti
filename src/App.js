a9ac40d072f1824c4357f3b50465409041051a51
Christian Francini Mascagni <59649715+Chri2187@users.noreply.github.com>
2022-03-01 20:15:53 +0100

    Major Update
    creazione state per utilizzo dati fake, creazione funzioni per reload e delete degli appuntamenti

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
+import List from "./List";
+import data from "./data";
+import { useState } from "react";
 
 function App() {
+  const [people, setPeople] = useState(data);
+  const reloadAllItem = () => {
+    setPeople(data);
+  };
+
+  const removeItem = (id) => {
+    setPeople((oldValue) => {
+      return oldValue.filter((value) => value.id !== id);
+    });
+  };
   return (
-    <div className="App">
-    </div>
+    <section>
+      <div className="container">
+        <h2 style={{ color: "var(--bg-blue)" }}>Prossimi Appuntamenti</h2>
+        <div className="people-list">
+          <List data={people} removeItem={removeItem} />
+        </div>
+        <div className="btn-group">
+          <button className="btn btn-reset" onClick={reloadAllItem}>
+            Reset
+          </button>
+          <button className="btn btn-delete" onClick={() => setPeople([])}>
+            Delete All
+          </button>
+        </div>
+      </div>
+    </section>
   );
 }
 
--- src/List.js
+++ src/List.js
@@ -1,11 +1,32 @@
 import React from "react";
-
-const List = () => {
-  return <ul></ul>;
+import { FiDelete } from "react-icons/fi";
+const List = (props) => {
+  return (
+    <ul className="user-list">
+      {props.data.map((person) => (
+        <li key={person.id}>
+          <Person {...person} removeItem={props.removeItem} />
+        </li>
+      ))}
+    </ul>
+  );
 };
 
-const Person = () => {
-  return <article>persona</article>;
+const Person = ({ id, nome, stato, img, removeItem }) => {
+  return (
+    <article>
+      <img src={img} alt={nome} className="person-img" />
+      <div className="person-info">
+        <div className="person-action">
+          <h4>{nome}</h4>
+          <button className="btn">
+            <FiDelete className="icon" onClick={() => removeItem(id)} />
+          </button>
+        </div>
+        <p>{stato}</p>
+      </div>
+    </article>
+  );
 };
 
 export default List;
